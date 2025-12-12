2dc70665dc31cbcb105353be01cb3e0b54aa64af
deepakjangird414-cloud <deepakjangird414@gmail.com>
2025-12-12 10:08:05 +0530

    Add files via upload

---
+++ tutedude.py
@@ -0,0 +1,12 @@
+# task 1 tutedude
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+print("addition:", a + b)
+print("subtraction:", a - b)
+print("multiplication:", a * b)
+print("division:", a / b)
+
+# task 2 tutedude
+user_name = input("enter the first name:")
+user_name1 = input("enter the last name:")
+print("hello", user_name + " " + user_name1)
